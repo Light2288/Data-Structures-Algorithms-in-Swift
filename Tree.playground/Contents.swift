16af6689090d2335dc8ead52ea6168dc3bed7789
Light2288 <davide.aliti@gmail.com>
2022-06-21 01:41:35 +0200

    add tree node implementation add depth-first visit algorithm

--- Tree.playground/Contents.swift
+++ Tree.playground/Contents.swift
@@ -1,3 +1,50 @@
 import UIKit
 
-var greeting = "Hello, playground"
+func example(of description: String, completion: () -> Void) {
+    print("\n\nExample of \(description)\n")
+    completion()
+}
+
+func makeBeverageTree() -> TreeNode<String> {
+    let tree = TreeNode("Beverages")
+    let hot = TreeNode("Hot")
+    let cold = TreeNode("Cold")
+    let tea = TreeNode("Tea")
+    let coffee = TreeNode("Coffee")
+    let chocolate = TreeNode("Chocolate")
+    let blackTea = TreeNode("Black Tea")
+    let greenTea = TreeNode("Green Tea")
+    let chaiTea = TreeNode("Chai Tea")
+    let soda = TreeNode("Soda")
+    let milk = TreeNode("Milk")
+    let gingerAle = TreeNode("Ginger Ale")
+    let bitterLemon = TreeNode("Bitter Lemon")
+    
+    tree.add(hot)
+    tree.add(cold)
+    hot.add(tea)
+    hot.add(coffee)
+    hot.add(chocolate)
+    cold.add(soda)
+    cold.add(milk)
+    tea.add(blackTea)
+    tea.add(greenTea)
+    tea.add(chaiTea)
+    soda.add(gingerAle)
+    soda.add(bitterLemon)
+    
+    return tree
+}
+
+example(of: "creating a tree") {
+    let beverages = TreeNode("Beverages")
+    let hot = TreeNode("Hot")
+    let cold = TreeNode("Cold")
+    beverages.add(hot)
+    beverages.add(cold)
+}
+
+example(of: "depth-first traversal") {
+    let tree = makeBeverageTree()
+    tree.forEachDepthFirst { print($0.value) }
+}
---
+++ Tree.playground/Sources/TreeNode.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+public class TreeNode<T> {
+    public var value: T
+    public var children: [TreeNode] = []
+    
+    public init(_ value: T) {
+        self.value = value
+    }
+    
+    public func add(_ child: TreeNode) {
+        self.children.append(child)
+    }
+}
+
+extension TreeNode {
+    public func forEachDepthFirst(visit: (TreeNode) -> Void) {
+        visit(self)
+        children.forEach {
+            $0.forEachDepthFirst(visit: visit)
+        }
+    }
+}
